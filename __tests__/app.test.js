3f8d6e2b5e51b82a4526426c692df6b6391b5db9
Tajwar0 <tajwar_h@hotmail.com>
2022-07-04 15:57:34 +0100

    implement an endpoint which responds with a specific article requested by ID

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,19 +6,36 @@ const testData = require("../db/data/test-data/index");
 beforeEach(() => seed(testData));
 afterAll(() => connection.end());
 
-const topics = [
-  { description: "The man, the Mitch, the legend", slug: "mitch" },
-  { description: "Not dogs", slug: "cats" },
-  { description: "what books are made of", slug: "paper" },
+const neededKeys = [
+  "author",
+  "title",
+  "article_id",
+  "body",
+  "topic",
+  "created_at",
+  "votes",
 ];
 
-describe("3 GET api topics", () => {
-  it("respond with an array of topic objects, including properties'slug' and 'description ", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then(({ body }) => {
-        expect(body).toEqual(topics);
-      });
+describe("Name of the group", () => {
+  describe("3 GET api topics", () => {
+    it("respond with an array of topic objects, including properties'slug' and 'description ", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toEqual(testData.topicData);
+        });
+    });
+  });
+
+  describe("GET /api/articles/:article_id", () => {
+    it("responds with an article object with the following properties: author, title, article_id, body, topic, created_at, votes", () => {
+      return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({ body }) => {
+          expect(neededKeys.every((key) => key in body)).toEqual(true);
+        });
+    });
   });
 });
